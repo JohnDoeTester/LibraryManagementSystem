7f4fcb2e6e598db730bed91eec28f25fbaf3dd51
JohnDoeTester <tester.donotreply.tester@gmail.com>
2023-04-25 17:03:35 +0530

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -3,8 +3,8 @@
 session_start();
 
 define('LOCALHOST', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', 'password');
 define('DB_NAME', 'library_system');
 
 
